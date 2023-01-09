81470d9b3daa7d9bd6d4eeb7da2958893bbba41a
Kamal <dearkamalpreet@gmail.com>
2023-01-09 14:48:03 +0100

    address updated

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosApiInstance = axios.create();
 
-const url = process.env.NODE_ENV === "development"? "http://localhot:5000": "https://user-api.onrender.com";
+const url = process.env.NODE_ENV === "development"? "http://localhost:5000": "https://user-api-czc0.onrender.com";
 
 axiosApiInstance.defaults.baseURL = url;
 axiosApiInstance.defaults.withCredentials = true;
